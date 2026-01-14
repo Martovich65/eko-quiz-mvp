a0fdbe12cfba0ebc4674da3f99fa4639fff2208b
Martovich65 <gordiyenkokot65@gmail.com>
2026-01-14 18:13:26 +0200

    Update auth.js

--- pages/api/pages/api/auth.js
+++ pages/api/pages/api/auth.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+  const shop = req.query.shop;
+
+  if (!shop) {
+    return res.status(400).send("Missing ?shop parameter");
+  }
+
+  const redirectUri = `https://${req.headers.host}/api/auth/callback`;
+
+  const installUrl =
+    `https://${shop}/admin/oauth/authorize` +
+    `?client_id=${process.env.SHOPIFY_CLIENT_ID}` +
+    `&scope=read_customers,write_customers,read_marketing_events,write_marketing_events` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  res.redirect(installUrl);
+}
 
